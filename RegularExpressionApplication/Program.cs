ca194d2e7bcca561cb63e02a5d37b1e09ddb0164
tayutaedomo <tayutaedomo@gmail.com>
2017-02-14 16:55:15 +0900

    Update RegularExpressionApplication

--- RegularExpressionApplication/Program.cs
+++ RegularExpressionApplication/Program.cs
@@ -11,6 +11,7 @@ namespace RegularExpressionApplication
 		{
 			func();
 			func2();
+			func3();
 
 			sample();
 			sample2();
@@ -20,14 +21,14 @@ namespace RegularExpressionApplication
 		{
 			string str = "LS0MQxoT1ay8bQBtkBNb_abc.jpg";
 
-			Regex reg = new Regex(@"(?<a>[0-9a-zA-Z]{20})_.+\.(?<b>.+)");
+			Regex reg = new Regex(@"^([0-9a-zA-Z]{20})_.+\.(.+)$");
 
-            Console.WriteLine(reg.IsMatch(str));
+			Console.WriteLine(reg.IsMatch(str));
 
 			MatchCollection mc = reg.Matches(str);
 
-            Console.WriteLine(mc.Count);
-            Console.WriteLine(mc[0].Value);
+			Console.WriteLine(mc.Count);
+			Console.WriteLine(mc[0].Value);
 
 			//if (mc.Count > 0)
 			if (mc.Count > 0)
@@ -35,7 +36,7 @@ namespace RegularExpressionApplication
 				GroupCollection group = mc[0].Groups;
 				for (int i = 0; i < group.Count; i++)
 				{
-            		Console.WriteLine(i + ": " + group[i].Value);
+					Console.WriteLine(i + ": " + group[i].Value);
 				}
 			}
 		}
@@ -46,7 +47,7 @@ namespace RegularExpressionApplication
 
 			string str = "LS0MQxoT1ay8bQBtkBNb_abc.jpg";
 
-			Regex reg = new Regex(@"(?<a>[0-9a-zA-Z]{20})_.+\.(?<b>.+)");
+			Regex reg = new Regex(@"^([0-9a-zA-Z]{20})_.+\.(.+)$");
 
 			Match match = reg.Match(str);
 
@@ -55,11 +56,41 @@ namespace RegularExpressionApplication
 				GroupCollection groups = match.Groups;
 				for (int i = 0; i < groups.Count; i++)
 				{
-            		Console.WriteLine(i + ": " + groups[i].Value);
+					Console.WriteLine(i + ": " + groups[i].Value);
 				}
 
 				match = match.NextMatch();
 			}
+		}
+
+		public static void func3() {
+			var str = "LS0MQxoT1ay8bQBtkBNb_abc.jpg";
+
+			var items = extract_file_name_items(str);
+
+			foreach (var item in items) {
+				Console.WriteLine(item);
+			}
+		}
+
+		public static List<string> extract_file_name_items(string file_name) {
+			var items = new List<string>();
+
+			Regex reg = new Regex(@"^([0-9a-zA-Z]{10})([0-9a-zA-Z]{10})_.+\.(.+)$");
+
+			MatchCollection mc = reg.Matches(file_name);
+
+			if (mc.Count > 0) {
+				GroupCollection group = mc[0].Groups;
+
+				if (group.Count > 3) {
+					items.Add(group[1].Value);
+					items.Add(group[2].Value);
+					items.Add(group[3].Value);
+				}
+			}
+
+			return items;
 		}
 
 		public static void sample()
@@ -68,7 +99,7 @@ namespace RegularExpressionApplication
 
 			Regex reg = new Regex("[a-z]");
 			string ui = reg.Replace("123abc456vrtldef789", "");
-            Console.WriteLine(ui);
+			Console.WriteLine(ui);
 		}
 
 		public static void sample2()
@@ -78,9 +109,9 @@ namespace RegularExpressionApplication
 //			string time = "";
 //			MatchCollection mc;
 //			mc = Regex.Matches (time, @"\d{1,}(?=時)|\d{1,}(?=分)|\d\d*\.\d{1,}(?=秒)");
-//            foreach (Match item in mc) {
-//                print (string.Format ("{0}:{1}", item.Value, item.Index));
-//            }
+//			foreach (Match item in mc) {
+//				print (string.Format ("{0}:{1}", item.Value, item.Index));
+//			}
 //			minuteHand = int.Parse (mc [0].Value);
 //			hourHand = int.Parse (mc [1].Value);
 		}
